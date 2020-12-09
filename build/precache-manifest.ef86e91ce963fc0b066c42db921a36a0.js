59d91406002cf2383ad84191c06d4013f3f8a027
rmusman <usman@hashlogics.com>
2020-12-09 17:00:05 +0500

    express added

---
+++ build/precache-manifest.ef86e91ce963fc0b066c42db921a36a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4544b960783ae3cd6259f4333f61495d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2dfe5ddf95943cfed8",
+    "url": "/static/css/2.377bb7e4.chunk.css"
+  },
+  {
+    "revision": "5453269f1c3d0af6c0bb",
+    "url": "/static/css/main.66a100f8.chunk.css"
+  },
+  {
+    "revision": "6f2dfe5ddf95943cfed8",
+    "url": "/static/js/2.5972e410.chunk.js"
+  },
+  {
+    "revision": "4c65d4cdbc021411f1c6e583b95c594d",
+    "url": "/static/js/2.5972e410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5453269f1c3d0af6c0bb",
+    "url": "/static/js/main.7577e0c7.chunk.js"
+  },
+  {
+    "revision": "4e8d81c11a08d898b514",
+    "url": "/static/js/runtime-main.36464ef8.js"
+  },
+  {
+    "revision": "6ca8df74ac227213baa42f5404a3aea6",
+    "url": "/static/media/officialLogo.6ca8df74.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
