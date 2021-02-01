c66c261bd835d7ecd3e38f5e4cb6a99e5b922b16
Abdul Basit <basit850@gmail.com>
2021-02-01 17:45:59 +0500

    publicMusic

---
+++ build/precache-manifest.29acbbd306d8161b13c2ca1ffafe89ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0154c8386c1f8ae65f7d893efa55c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6462fbbe77914e1e7a9",
+    "url": "/static/css/2.377bb7e4.chunk.css"
+  },
+  {
+    "revision": "c7d000d5bf727801bcc0",
+    "url": "/static/css/main.8882048b.chunk.css"
+  },
+  {
+    "revision": "c6462fbbe77914e1e7a9",
+    "url": "/static/js/2.bb5fdba8.chunk.js"
+  },
+  {
+    "revision": "2b4f56a64a4b487a64344affc3175e56",
+    "url": "/static/js/2.bb5fdba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d000d5bf727801bcc0",
+    "url": "/static/js/main.f0a18c4e.chunk.js"
+  },
+  {
+    "revision": "4e8d81c11a08d898b514",
+    "url": "/static/js/runtime-main.36464ef8.js"
+  },
+  {
+    "revision": "6ca8df74ac227213baa42f5404a3aea6",
+    "url": "/static/media/officialLogo.6ca8df74.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
